88acab1e5a8e57d1c22296c1f20ff9114908537d
Zadanka <62470630+Zadanka@users.noreply.github.com>
2020-03-21 23:15:20 +0100

    Add files via upload

---
+++ zad10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+float suma(float a, float b)
+{
+	float wynik;
+	wynik=a+b;
+}
+
+int main()
+{
+	float a,b;
+	cout<<"Suma wynosi: "<<suma(10, 5);
+	return 0;
+}
