eca4261fb808cd1b4a990973f6b98e41f01466ef
Daniel <spythere@protonmail.com>
2020-03-21 21:54:45 +0100

    Dodanie delete[] na końcu pliku

--- zad3.cpp
+++ zad3.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
- 
+
 using namespace std;
- 
+
 double *dodaj_1(double *tab, int rozmiar)
 {
     double *nowa_tab = new double[rozmiar];
- 
+
     for (int i = 0; i < rozmiar; i++)
-            nowa_tab[i] = tab[i]+1;
- 
+        nowa_tab[i] = tab[i] + 1;
+
     return nowa_tab;
 }
- 
+
 int main()
 {
     double tab[6] = {1, 3, 4, 5, 10, 12};
- 
+
     double *nowa = dodaj_1(tab, 6);
- 
+
     for (int i = 0; i < 6; i++)
     {
         cout << nowa[i] << endl;
     }
- 
+
+    delete[] nowa;
+
     return 0;
 }
