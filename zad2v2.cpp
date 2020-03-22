be4d7de3752c3856bd2f4566962444ed6959c2c1
cisek <krzysieknarzedzia@gmail.com>
2020-03-22 11:06:36 +0100

    11-13

--- zad2v2.cpp
+++ zad2v2.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
- 
+
 using namespace std;
- 
-double *kasuj_element(double *tab, int rozmiar, int ktory)
+
+double *odwroc(double *tab, int rozmiar)
 {
-    double *nowa_tab = new double[rozmiar - 1];
- 
+    double *nowa_tab = new double[rozmiar];
+
     for (int i = 0; i < rozmiar; i++)
     {
-        if (i == ktory)
-            continue;
- 
-        if (i < ktory)
-            nowa_tab[i] = tab[i];
- 
-        if (i > ktory)
-            nowa_tab[i - 1] = tab[i];
+        nowa_tab[i] = tab[9-i];
     }
- 
+
     return nowa_tab;
 }
- 
+
 int main()
 {
-    double tab[6] = {1, 3, 4, 5, 10, 12};
- 
-    double *nowa = kasuj_element(tab, 6, 5);
- 
-    for (int i = 0; i < 5; i++)
+    double tab[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double *nowa = odwroc(tab, 10);
+
+    for (int i = 0; i < 10; i++)
     {
         cout << nowa[i] << endl;
     }
- 
     return 0;
 }
